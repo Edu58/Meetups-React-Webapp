2298b1777f3b8770c54d64e7dfe3bfa8da4d6bd7
Edwin Karimi <edumuriithi58@gmail.com>
2022-02-16 10:24:36 +0300

    added useRef in NewMeetUps to access form input

--- src/pages/NewMeetUps.js
+++ src/pages/NewMeetUps.js
@@ -1,25 +1,51 @@
-import Button from '../components/UI/Button';
-import Card from '../components/UI/Card'
-import classes from './NewMeetUps.module.css'
+import { useRef } from "react/cjs/react.development";
+import Button from "../components/UI/Button";
+import Card from "../components/UI/Card";
+import classes from "./NewMeetUps.module.css";
 
 function NewMeetUps() {
+  const meetUpNameInputref = useRef();
+  const addressInputref = useRef();
+  const imageUrlInputref = useRef();
+  const descriptionInputref = useRef();
+
+  function SubmitHandler(event) {
+    event.preventDefault();
+
+    const meetUpName = meetUpNameInputref.current.value;
+    const addressName = addressInputref.current.value;
+    const imageUrl = imageUrlInputref.current.value;
+    const descriptionInput = descriptionInputref.current.value;
+
+    const newmeetUpData = {
+      title: meetUpName,
+      address: addressName,
+      image: imageUrl,
+      description: descriptionInput
+    }
+  }
+
   return (
     <Card>
       <div className={classes.container}>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={SubmitHandler}>
           <h3 className={classes.title}>Add meetUp</h3>
 
           <label htmlFor="meetup_title">Add meetUp Name</label>
-          <input id="meetup_title"></input>
+          <input id="meetup_title" ref={meetUpNameInputref}></input>
 
-          <label htmlFor="meetup_location">Place Name</label>
-          <input id="meetup_location"></input>
+          <label htmlFor="meetup_location">Address</label>
+          <input id="meetup_location" ref={addressInputref}></input>
 
           <label htmlFor="meetup_image">Image URL</label>
-          <input id="meetup_image"></input>
+          <input id="meetup_image" ref={imageUrlInputref}></input>
 
           <label htmlFor="meetup_description">Description</label>
-          <textarea rows={8} id="meetup_description"></textarea>
+          <textarea
+            rows={8}
+            id="meetup_description"
+            ref={descriptionInputref}
+          ></textarea>
 
           <Button>Add New meetUp</Button>
         </form>
